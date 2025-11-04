cb05d121022f1138dd35530e9b4e7f776716be6d
MUHAMMED NIHAL <145740029+nihalzown@users.noreply.github.com>
2025-11-04 15:53:10 +0530

    update

--- s5-class/cryptolab/mixed/md5_dsa.java
+++ s5-class/cryptolab/mixed/md5_dsa.java
@@ -16,7 +16,27 @@ public class md5_dsa {
         }
         System.out.println("hex :"+hex);
 
-        
+        KeyPairGenerator keypgen = KeyPairGenerator.getInstance("DSA");
+        keypgen.initialize(2048);
+        KeyPair keys = keypgen.generateKeyPair();
+        PrivateKey pri = keys.getPrivate();
+        PublicKey pub = keys.getPublic();
+
+        System.out.println("DSA Private key :"+ Base64.getEncoder().encodeToString(pri.getEncoded()));
+        System.out.println("DSA Public key :"+ Base64.getEncoder().encodeToString(pub.getEncoded()));
+
+        Signature sig = Signature.getInstance("SHA256withDSA");
+        sig.initSign(pri);
+        sig.update(digest);
+        byte[] digital = sig.sign();
+        System.out.println("Digital Signature : "+Base64.getEncoder().encodeToString(digital));
+
+        sig.initVerify(pub);
+        sig.update(digest);
+        boolean isverified = sig.verify(digital);
+        System.out.println("Signature verified :"+isverified);
+
+        sc.close();
 
     }    
 }
