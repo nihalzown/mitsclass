04de4b4e3b6d4ca69959e0974b43a9fbed693bb1
MUHAMMED NIHAL <145740029+nihalzown@users.noreply.github.com>
2025-11-04 19:44:03 +0530

    updated

---
+++ s5-class/cryptolab/labquestions/substitution.java
@@ -0,0 +1,50 @@
+/*a) Write a Java program that implements a simple letter-mapping cipher, 
+where each letter of the alphabet is substituted by another based on a given key. 
+Use the key where the letter 'A' is replaced by 'Q', 'B' by 'W', and so on. 
+Encrypt and then decrypt the phrase "PRIVATE".   */
+
+
+import java.util.*;
+public class substitution{
+    private static String alfa = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    public static String encrypt(String text ,String key){
+        text = text.toUpperCase();
+        StringBuilder ct = new StringBuilder();
+        for(char c : text.toCharArray()){
+            int idx = alfa.indexOf(c);
+            if(idx!=-1){
+                ct.append(key.charAt(idx));
+            }
+            else{
+                    ct.append(c);
+            }
+        }
+        return ct.toString();
+    }
+    public static String decrypt(String ct ,String key){
+        ct.toUpperCase();
+        StringBuilder pt = new StringBuilder();
+        for(char c : ct.toCharArray()){
+            int idx = key.indexOf(c);
+            if(idx!=-1){
+                pt.append(alfa.charAt(idx));
+            }
+            else{
+                    pt.append(c);
+            }
+        }
+        return pt.toString();
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String key = "QWERTYUIOPASDFGHJKLZXCVBNM";
+        key.toUpperCase();
+        String text = "PRIVATE";
+        text.toUpperCase();
+        String monoencrypted = encrypt(text, key);
+        String monodecrypted = decrypt(monoencrypted, key);
+        System.out.println("Encrypted: " + monoencrypted);
+        System.out.println("Decrypted: " + monodecrypted);
+
+    }
+}
