c8c5aa3a8d3833057e7217132e68ecf3b028e615
MUHAMMED NIHAL <145740029+nihalzown@users.noreply.github.com>
2025-11-04 15:40:28 +0530

    update

---
+++ s5-class/cryptolab/mixed/md5_dsa.java
@@ -0,0 +1,22 @@
+import java.util.*;
+import java.security.*;
+
+public class md5_dsa {
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("enter the message :");
+        String text = sc.nextLine();
+        MessageDigest m = MessageDigest.getInstance("MD5");
+        byte[] digest = m.digest(text.getBytes());
+        System.out.println("MD5 :"+Base64.getEncoder().encodeToString(digest));
+        StringBuilder hex = new StringBuilder();
+        for(byte b : digest){
+            String h =String.format("%2x",b);
+            hex.append(h);
+        }
+        System.out.println("hex :"+hex);
+
+        
+
+    }    
+}
